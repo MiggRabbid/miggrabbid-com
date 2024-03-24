b08342e37d390a93157086cc1a17488842ed0375
MiggRabbid <mig26@inbox.ru>
2024-03-24 21:05:48 +0300

    DEV: Made an infinite scroll stack

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -3,6 +3,9 @@ import styles from './About.module.scss';
 
 const About = () => (
   <section className={styles.container}>
+    <div className={styles.title}>
+      <h2>About me</h2>
+    </div>
     <div className={styles.about}>
       <p>
         My journey in programming began in 2021. Thanks to YouTube recommendations, I completed a short introductory course on the fundamentals of &quot;Python&quot;, and it changed my life.
